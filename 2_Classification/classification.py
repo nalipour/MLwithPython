f71681fb5e5393c3283464bd6802435af19ef898
Niloufar Alipour Tehrani <nalipour@cern.ch>
2018-03-23 17:49:17 +0100

    Multiclass classification

--- 2_Classification/classification.py
+++ 2_Classification/classification.py
@@ -8,7 +8,10 @@ from sklearn.base import BaseEstimator
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_score, recall_score, f1_score
-from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import precision_recall_curve, roc_curve, roc_auc_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.preprocessing import StandardScaler
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -119,3 +122,105 @@ plt.plot(recalls, precisions)
 y_train_pred_90 = (y_scores > 70000)
 precision_score(y_train_5, y_train_pred_90)
 recall_score(y_train_5, y_train_pred_90)
+
+
+# ROC Curve: Receiver Operating Characteristic
+fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+
+
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.axis([0, 1, 0, 1])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+
+
+plot_roc_curve(fpr, tpr)
+plt.show()
+roc_auc_score(y_train_5, y_scores)
+
+forest_clf = RandomForestClassifier(random_state=42)
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,
+                                    method='predict_proba')
+y_scores_forest = y_probas_forest[:, 1]
+fpr_forest, tpr_forest, thresholds_forest = roc_curve(
+    y_train_5, y_scores_forest)
+
+plt.plot(fpr, tpr, 'b:', label='SGD')
+plot_roc_curve(fpr_forest, tpr_forest, 'Random Forest')
+plt.legend(loc='lower right')
+plt.show()
+roc_auc_score(y_train_5, y_scores_forest)
+
+
+# ####### Multiclass Classification ####### #
+sgd_clf.fit(X_train, y_train)
+sgd_clf.predict([some_digit])
+some_digit_scores = sgd_clf.decision_function([some_digit])
+np.argmax(some_digit_scores)
+sgd_clf.classes_
+sgd_clf.classes_[0]
+
+ovo_clf = OneVsOneClassifier(SGDClassifier(random_state=42))
+ovo_clf.fit(X_train, y_train)
+ovo_clf.predict([some_digit])
+len(ovo_clf.estimators_)
+
+forest_clf.fit(X_train, y_train)
+forest_clf.predict([some_digit])
+forest_clf.predict_proba([some_digit])
+
+cross_val_score(sgd_clf, X_train, y_train, cv=3, scoring='accuracy')
+
+# Scaling for a better accuracy
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train.astype(np.float64))
+cross_val_score(sgd_clf, X_train_scaled, y_train, cv=3, scoring='accuracy')
+
+# ####### Error Analysis ####### #
+y_train_pred = cross_val_predict(sgd_clf, X_train_scaled, y_train, cv=3)
+conf_mx = confusion_matrix(y_train, y_train_pred)
+plt.matshow(conf_mx, cmap=plt.cm.gray)
+plt.show()
+row_sums = conf_mx.sum(axis=1, keepdims=True)
+norm_conf_mx = conf_mx / row_sums
+
+# Rows: Actual classes
+# Columns: Predicted classes
+np.fill_diagonal(norm_conf_mx, 0)
+plt.matshow(norm_conf_mx, cmap=plt.cm.gray)
+
+
+def plot_digits(instances, images_per_row=10, **options):
+    size = 28
+    images_per_row = min(len(instances), images_per_row)
+    images = [instance.reshape(size, size) for instance in instances]
+    n_rows = (len(instances) - 1) // images_per_row + 1
+    row_images = []
+    n_empty = n_rows * images_per_row - len(instances)
+    images.append(np.zeros((size, size * n_empty)))
+    for row in range(n_rows):
+        rimages = images[row * images_per_row: (row + 1) * images_per_row]
+        row_images.append(np.concatenate(rimages, axis=1))
+    image = np.concatenate(row_images, axis=0)
+    plt.imshow(image, cmap=matplotlib.cm.binary, **options)
+    plt.axis("off")
+
+
+cl_a, cl_b = 3, 5
+X_aa = X_train[(y_train == cl_a) & (y_train_pred == cl_a)]
+X_ab = X_train[(y_train == cl_a) & (y_train_pred == cl_b)]
+X_ba = X_train[(y_train == cl_b) & (y_train_pred == cl_a)]
+X_bb = X_train[(y_train == cl_b) & (y_train_pred == cl_b)]
+
+plt.figure(figsize=(8, 8))
+plt.subplot(221)
+plot_digits(X_aa[:25], images_per_row=5)
+plt.subplot(222)
+plot_digits(X_ab[:25], images_per_row=5)
+plt.subplot(223)
+plot_digits(X_ba[:25], images_per_row=5)
+plt.subplot(224)
+plot_digits(X_bb[:25], images_per_row=5)
+plt.show()
