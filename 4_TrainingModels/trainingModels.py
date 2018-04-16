6a9fe92f9f454b792fb7eb5ab02b62d13487d45a
Niloufar Alipour Tehrani <nalipour@cern.ch>
2018-04-16 16:56:15 +0200

    Normal equation, gradient descent (batch&stochastic)

--- 4_TrainingModels/trainingModels.py
+++ 4_TrainingModels/trainingModels.py
@@ -1,8 +1,66 @@
 import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import SGDRegressor
+
 import matplotlib
 import matplotlib.pyplot as plt
 %matplotlib inline
 
 
+# ## The Normal Equation ###
 X = 2 * np.random.rand(100, 1)
 y = 4 + 3 * X + np.random.randn(100, 1)
+plt.plot(X, y, 'o')
+
+X_b = np.c_[np.ones((100, 1)), X]  # add x0 = 1 to each instance
+theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
+X_new = np.array([[0], [2]])
+X_new_b = np.c_[np.ones((2, 1)), X_new]  # add x0 = 1 to each instance
+y_predict = X_new_b.dot(theta_best)
+plt.plot(X_new, y_predict, "r-")
+plt.plot(X, y, ".")
+plt.axis([0, 2, 0, 15])
+
+# Equivalent code in SciKit-Learn
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+lin_reg.intercept_, lin_reg.coef_
+lin_reg.predict(X_new)
+
+# #### Gradient Descent #### #
+eta = 0.1
+n_iterations = 1000
+m = 100
+
+theta = np.random.randn(2, 1)
+
+for iteration in range(n_iterations):
+    gradients = 2/m * X_b.T.dot(X_b.dot(theta)-y)
+    theta = theta - eta * gradients
+
+# #### Stochastic Gradient Descent #### #
+n_epochs = 50
+t0, t1, = 5, 50  # learning schedule hyperparameters
+
+
+def learning_schedule(t):
+    return t0 / (t + t1)
+
+
+theta = np.random.randn(2, 1)  # random initialisation
+
+for epoch in range(n_epochs):
+    for i in range(m):
+        random_index = np.random.randint(m)
+        xi = X_b[random_index:random_index+1]
+        yi = y[random_index:random_index+1]
+        gradients = 2 * xi.T.dot(xi.dot(theta)-yi)
+        eta = learning_schedule(epoch * m + i)
+        theta = theta - eta * gradients
+
+theta
+
+# Use SciKit - learn
+sgd_reg = SGDRegressor(n_iter=50, penalty=None, eta0=0.1)
+sgd_reg.fit(X, y.ravel())
+sgd_reg.intercept_, sgd_reg.coef_
