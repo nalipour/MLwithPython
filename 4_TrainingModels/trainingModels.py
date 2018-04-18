e226cf2dcfe99e42c42be487f6b39b2fc126e8c1
Niloufar Alipour Tehrani <nalipour@cern.ch>
2018-04-18 16:22:00 +0200

    Regularisation

--- 4_TrainingModels/trainingModels.py
+++ 4_TrainingModels/trainingModels.py
@@ -1,6 +1,18 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import SGDRegressor
+from sklearn.preprocessing import PolynomialFeatures
+
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+from sklearn.pipeline import Pipeline
+
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.base import clone
+from sklearn.preprocessing import StandardScaler
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -64,3 +76,105 @@ theta
 sgd_reg = SGDRegressor(n_iter=50, penalty=None, eta0=0.1)
 sgd_reg.fit(X, y.ravel())
 sgd_reg.intercept_, sgd_reg.coef_
+
+
+# #### Polynomial Regression #### #
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 0.5 * X**2 + X + 2 + np.random.randn(m, 1)
+plt.plot(X, y, '.')
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_poly = poly_features.fit_transform(X)  # X_poly contains both X and X**2
+X[0]
+X_poly[0]
+lin_reg.fit(X_poly, y)
+lin_reg.intercept_, lin_reg.coef_
+X_new = np.linspace(-3, 3, 100).reshape(100, 1)
+X_new_poly = poly_features.transform(X_new)
+y_new = lin_reg.predict(X_new_poly)
+plt.plot(X, y, "b.")
+plt.plot(X_new, y_new, "r-", linewidth=2, label="Predictions")
+
+
+def plot_learning_curves(model, X, y):
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+    train_errors, val_errors = [], []
+    for m in range(1, len(X_train)):
+        model.fit(X_train[:m], y_train[:m])
+        y_train_predict = model.predict(X_train[:m])
+        y_val_predict = model.predict(X_val)
+        train_errors.append(mean_squared_error(y_train_predict, y_train[:m]))
+        val_errors.append(mean_squared_error(y_val_predict, y_val))
+    plt.plot(np.sqrt(train_errors), 'r-+', linewidth=2, label='train')
+    plt.plot(np.sqrt(val_errors), 'b-', linewidth=3, label='val')
+
+
+lin_reg = LinearRegression()
+plot_learning_curves(lin_reg, X, y)
+
+polynomial_regression = Pipeline((
+    ('poly_features', PolynomialFeatures(degree=10, include_bias=False)),
+    ('lin_reg', LinearRegression())
+))
+
+plot_learning_curves(polynomial_regression, X, y)
+plt.axis([0, 80, 0, 3])
+
+
+# ########## Regularized Linear Models ########## #
+# ### Ridge Regression ### #
+ridge_reg = Ridge(alpha=1, solver='cholesky')
+ridge_reg.fit(X, y)
+ridge_reg.predict([[1.5]])
+
+sgd_reg = SGDRegressor(penalty="l2")  # l2 is the Redge Regressor
+sgd_reg.fit(X, y.ravel())
+sgd_reg.predict([[1.5]])
+
+
+# ### Lasso Regression ### #
+# Or SGDRegressor(penalty='l1')
+lasso_reg = Lasso(alpha=0.1)
+lasso_reg.fit(X, y)
+lasso_reg.predict([[1.5]])
+
+
+# ### Elastic Net ### #
+elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
+elastic_net.fit(X, y)
+elastic_net.predict([[1.5]])
+
+
+# ### Early Stopping ### # (See the author's
+# GitHub for the full implementation)
+np.random.seed(42)
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 2 + X + 0.5 * X**2 + np.random.randn(m, 1)
+
+X_train, X_val, y_train, y_val = train_test_split(X[:50], y[:50].ravel(),
+                                                  test_size=0.5,
+                                                  random_state=10)
+
+poly_scaler = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=90, include_bias=False)),
+        ("std_scaler", StandardScaler()),
+    ])
+
+X_train_poly_scaled = poly_scaler.fit_transform(X_train)
+X_val_poly_scaled = poly_scaler.transform(X_val)
+
+sgd_reg = SGDRegressor(n_iter=1, warm_start=True, penalty=None,
+                       learning_rate='constant', eta0=0.0005)
+
+minimum_val_error = float('inf')
+best_epoch = None
+best_model = None
+for epoch in range(1000):
+    sgd_reg.fit(X_train_poly_scaled, y_train)
+    y_val_predict = sgd_reg.predict(X_val_poly_scaled)
+    val_error = mean_squared_error(y_val_predict, y_val)
+    if val_error < minimum_val_error:
+        minimum_val_error = val_error
+        best_epoch = epoch
+        best_model = clone(sgd_reg)
