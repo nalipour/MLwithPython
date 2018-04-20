2f985c9bab6769b3b39e5d59eedd8aadaa168969
Niloufar Alipour Tehrani <nalipour@cern.ch>
2018-04-20 07:47:56 +0200

    Nonlinear SVM classification

--- 5_SupportVectorMachines/SVM.py
+++ 5_SupportVectorMachines/SVM.py
@@ -4,6 +4,10 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 
+import matplotlib
+import matplotlib.pyplot as plt
+%matplotlib inline
+
 iris = datasets.load_iris()
 iris.keys()
 X = iris['data'][:, (2, 3)]  # petal length, petal width
@@ -18,3 +22,36 @@ svm_clf = Pipeline((
 
 svm_clf.fit(X, y)
 svm_clf.predict([[5.5, 1.7]])
+
+
+# ### Nonlinear SVM Classification ### #
+X1D = np.linspace(-4, 4, 9).reshape(-1, 1)
+X2D = np.c_[X1D, X1D**2]
+y = np.array([0, 0, 1, 1, 1, 1, 1, 0, 0])
+
+plt.figure(figsize=(11, 4))
+
+plt.subplot(121)
+plt.grid(True, which='both')
+plt.axhline(y=0, color='k')
+plt.plot(X1D[:, 0][y == 0], np.zeros(4), "bs")
+plt.plot(X1D[:, 0][y == 1], np.zeros(5), "g^")
+plt.gca().get_yaxis().set_ticks([])
+plt.xlabel(r"$x_1$", fontsize=20)
+plt.axis([-4.5, 4.5, -0.2, 0.2])
+
+plt.subplot(122)
+plt.grid(True, which='both')
+plt.axhline(y=0, color='k')
+plt.axvline(x=0, color='k')
+plt.plot(X2D[:, 0][y == 0], X2D[:, 1][y == 0], "bs")
+plt.plot(X2D[:, 0][y == 1], X2D[:, 1][y == 1], "g^")
+plt.xlabel(r"$x_1$", fontsize=20)
+plt.ylabel(r"$x_2$", fontsize=20, rotation=0)
+plt.gca().get_yaxis().set_ticks([0, 4, 8, 12, 16])
+plt.plot([-4.5, 4.5], [6.5, 6.5], "r--", linewidth=3)
+plt.axis([-4.5, 4.5, -1, 17])
+
+plt.subplots_adjust(right=1)
+
+plt.show()
