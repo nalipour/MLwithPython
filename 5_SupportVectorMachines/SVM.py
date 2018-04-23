6958b30e71e27280e70502d1e3798720b330ed7d
Niloufar Alipour Tehrani <nalipour@cern.ch>
2018-04-23 14:51:16 +0200

    SVM Regression

--- 5_SupportVectorMachines/SVM.py
+++ 5_SupportVectorMachines/SVM.py
@@ -5,6 +5,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVR
+from sklearn.svm import SVR
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -104,3 +106,88 @@ rbf_kernel_svm_clf = Pipeline((
 rbf_kernel_svm_clf.fit(X, y)
 plot_predictions(rbf_kernel_svm_clf, [-1.5, 2.5, -1, 1.5])
 plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
+
+# ### SVM Regression ### #
+# random data set
+np.random.seed(42)
+m = 50
+X = 2 * np.random.rand(m, 1)
+y = (4 + 3 * X + np.random.randn(m, 1)).ravel()
+
+svm_reg = LinearSVR(epsilon=1.5)
+svm_reg.fit(X, y)
+
+svm_reg1 = LinearSVR(epsilon=1.5, random_state=42)
+svm_reg2 = LinearSVR(epsilon=0.5, random_state=42)
+svm_reg1.fit(X, y)
+svm_reg2.fit(X, y)
+
+
+def find_support_vectors(svm_reg, X, y):
+    y_pred = svm_reg.predict(X)
+    off_margin = (np.abs(y - y_pred) >= svm_reg.epsilon)
+    return np.argwhere(off_margin)
+
+
+svm_reg1.support_ = find_support_vectors(svm_reg1, X, y)
+svm_reg2.support_ = find_support_vectors(svm_reg2, X, y)
+
+eps_x1 = 1
+eps_y_pred = svm_reg1.predict([[eps_x1]])
+
+
+def plot_svm_regression(svm_reg, X, y, axes):
+    x1s = np.linspace(axes[0], axes[1], 100).reshape(100, 1)
+    y_pred = svm_reg.predict(x1s)
+    plt.plot(x1s, y_pred, "k-", linewidth=2, label=r"$\hat{y}$")
+    plt.plot(x1s, y_pred + svm_reg.epsilon, "k--")
+    plt.plot(x1s, y_pred - svm_reg.epsilon, "k--")
+    plt.scatter(X[svm_reg.support_], y[svm_reg.support_],
+                s=180, facecolors='#FFAAAA')
+    plt.plot(X, y, "bo")
+    plt.xlabel(r"$x_1$", fontsize=18)
+    plt.legend(loc="upper left", fontsize=18)
+    plt.axis(axes)
+
+
+plt.figure(figsize=(9, 4))
+plt.subplot(121)
+plot_svm_regression(svm_reg1, X, y, [0, 2, 3, 11])
+plt.title(r"$\epsilon = {}$".format(svm_reg1.epsilon), fontsize=18)
+plt.ylabel(r"$y$", fontsize=18, rotation=0)
+
+plt.annotate(
+    '', xy=(eps_x1, eps_y_pred), xycoords='data',
+    xytext=(eps_x1, eps_y_pred - svm_reg1.epsilon),
+    textcoords='data', arrowprops={'arrowstyle': '<->', 'linewidth': 1.5}
+)
+plt.text(0.91, 5.6, r"$\epsilon$", fontsize=20)
+plt.subplot(122)
+plot_svm_regression(svm_reg2, X, y, [0, 2, 3, 11])
+plt.title(r"$\epsilon = {}$".format(svm_reg2.epsilon), fontsize=18)
+
+plt.show()
+
+np.random.seed(42)
+m = 100
+X = 2 * np.random.rand(m, 1) - 1
+y = (0.2 + 0.1 * X + 0.5 * X**2 + np.random.randn(m, 1)/10).ravel()
+
+svm_poly_reg = SVR(kernel='poly', degree=2, C=100, epsilon=0.1)
+svm_poly_reg.fit(X, y)
+svm_poly_reg1 = SVR(kernel="poly", degree=2, C=100, epsilon=0.1)
+svm_poly_reg2 = SVR(kernel="poly", degree=2, C=0.01, epsilon=0.1)
+svm_poly_reg1.fit(X, y)
+svm_poly_reg2.fit(X, y)
+
+plt.figure(figsize=(9, 4))
+plt.subplot(121)
+plot_svm_regression(svm_poly_reg1, X, y, [-1, 1, 0, 1])
+plt.title(r"$degree={}, C={}, \epsilon = {}$".format(
+    svm_poly_reg1.degree, svm_poly_reg1.C, svm_poly_reg1.epsilon), fontsize=18)
+plt.ylabel(r"$y$", fontsize=18, rotation=0)
+plt.subplot(122)
+plot_svm_regression(svm_poly_reg2, X, y, [-1, 1, 0, 1])
+plt.title(r"$degree={}, C={}, \epsilon = {}$".format(
+    svm_poly_reg2.degree, svm_poly_reg2.C, svm_poly_reg2.epsilon), fontsize=18)
+plt.show()
