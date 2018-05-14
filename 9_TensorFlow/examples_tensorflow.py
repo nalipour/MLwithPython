4ccf3205a0aebef5b5df7a1adc5bb2d18c42dc9c
Niloufar Alipour Tehrani <nalipour@cern.ch>
2018-05-14 10:24:33 +0200

    TensorBoard visualisation

--- 9_TensorFlow/examples_tensorflow.py
+++ 9_TensorFlow/examples_tensorflow.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from sklearn.datasets import fetch_california_housing
 from sklearn.preprocessing import StandardScaler
+from datetime import datetime
 
 dir(tf)
 x = tf.Variable(3, name='x')
@@ -148,3 +149,68 @@ with tf.Session() as sess:
     # Restore the graph's state
     saver.restore(sess, "tmp/my_model_final.ckpt")
     best_theta_restored = theta.eval()
+
+
+# ### TensorBoard ### #
+tf.reset_default_graph()
+now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+root_logdir = "tf_logs"
+logdir = "{}/run-{}/".format(root_logdir, now)
+
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.placeholder(tf.float32, shape=(None, n + 1), name='X')
+y = tf.placeholder(tf.float32, shape=(None, 1), name='y')
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42),
+                    name='theta')
+y_pred = tf.matmul(X, theta, name='predictions')
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name='mse')
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+mse_summary = tf.summary.scalar('MSE', mse)
+file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
+
+n_epochs = 10
+batch_size = 100
+n_batches = int(np.ceil(m / batch_size))
+
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        for batch_index in range(n_batches):
+            X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)
+            if batch_index % 10 == 0:
+                summary_str = mse_summary.eval(
+                    feed_dict={X: X_batch, y: y_batch})
+                step = epoch * n_batches + batch_index
+                file_writer.add_summary(summary_str, step)
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+
+    best_theta = theta.eval()
+file_writer.close()
+best_theta
+
+
+# ### Modularity ### #
+tf.reset_default_graph()
+
+
+def relu(X):
+    w_shape = (int(X.get_shape()[1]), 1)
+    w = tf.Variable(tf.random_normal(w_shape), name='weights')
+    b = tf.Variable(0.0, name='bias')
+    z = tf.add(tf.matmul(X, w), b, name='z')
+    return tf.maximum(z, 0., name='relu')
+
+
+n_features = 3
+X = tf.placeholder(tf.float32, shape=(None, n_features), name='X')
+relus = [relu(X) for i in range(5)]
+output = tf.add_n(relus, name='output')
+file_writer = tf.summary.FileWriter("logs/relu1", tf.get_default_graph())
