d1b75da9f12520d69b5b8a45f10d864ed9793bcd
Niloufar Alipour Tehrani <nalipour@cern.ch>
2018-05-14 20:23:29 +0200

    Sharing variables

--- 9_TensorFlow/examples_tensorflow.py
+++ 9_TensorFlow/examples_tensorflow.py
@@ -214,3 +214,27 @@ X = tf.placeholder(tf.float32, shape=(None, n_features), name='X')
 relus = [relu(X) for i in range(5)]
 output = tf.add_n(relus, name='output')
 file_writer = tf.summary.FileWriter("logs/relu1", tf.get_default_graph())
+
+# ### Sharing variables ### #
+tf.reset_default_graph()
+
+
+def relu(X):
+    threshold = tf.get_variable("threshold", shape=(),
+                                initializer=tf.constant_initializer(0.0))
+    w_shape = (int(X.get_shape()[1]), 1)
+    w = tf.Variable(tf.random_normal(w_shape), name="weights")
+    b = tf.Variable(0.0, name="bias")
+    z = tf.add(tf.matmul(X, w), b, name="z")
+    return tf.maximum(z, threshold, name="max")
+
+
+X = tf.placeholder(tf.float32, shape=(None, n_features), name="X")
+relus = []
+for relu_index in range(5):
+    with tf.variable_scope("relu", reuse=(relu_index >= 1)) as scope:
+        relus.append(relu(X))
+output = tf.add_n(relus, name="output")
+
+file_writer = tf.summary.FileWriter("logs/relu9", tf.get_default_graph())
+file_writer.close()
