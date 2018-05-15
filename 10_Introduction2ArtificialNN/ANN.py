bc528f84c044547265c3983a6817719acbbaae77
Niloufar Alipour Tehrani <nalipour@cern.ch>
2018-05-15 14:59:59 +0200

    Implementation of a DNN with plain TensorFlow

--- 10_Introduction2ArtificialNN/ANN.py
+++ 10_Introduction2ArtificialNN/ANN.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import Perceptron
+
 import numpy as np
 import tensorflow as tf
 
@@ -15,18 +16,16 @@ y_pred = per_clf.predict([[2, 0.5]])
 
 
 # ### FNN (Feedforward Neural Network) ### #
-# High-level API
+# MINST Data set
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
-np.shape(X_train)
 X_train = X_train.astype(np.float32).reshape(-1, 28 * 28) / 255.0
-np.shape(X_train)
 X_test = X_test.astype(np.float32).reshape(-1, 28 * 28) / 255.0
-np.shape(y_train)
 y_train = y_train.astype(np.int32)
 y_test = y_test.astype(np.int32)
 X_valid, X_train = X_train[:5000], X_train[5000:]
 y_valid, y_train = y_train[:5000], y_train[5000:]
 
+# ---- High-level API
 feature_cols = [tf.feature_column.numeric_column('X', shape=[28*28])]
 dnn_clf = tf.estimator.DNNClassifier(hidden_units=[300, 100], n_classes=10,
                                      feature_columns=feature_cols)
