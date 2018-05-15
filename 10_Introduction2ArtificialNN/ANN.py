454d03fd67fd0ac405f7e30281325d1fff0e95b4
Niloufar Alipour Tehrani <nalipour@cern.ch>
2018-05-15 10:25:32 +0200

    Perceptron

---
+++ 10_Introduction2ArtificialNN/ANN.py
@@ -0,0 +1,13 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Perceptron
+
+iris = load_iris()
+X = iris.data[:, (2, 3)]  # petal length and petal width
+y = (iris.target == 0).astype(np.int)  # Iris setosa ?
+
+# ### Perceptron ### #
+# Perceptron is equivalent to Stochastic Gradient Descent (SGDClassifier)
+per_clf = Perceptron(random_state=42)
+per_clf.fit(X, y)
+y_pred = per_clf.predict([[2, 0.5]])
