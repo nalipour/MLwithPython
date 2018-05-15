04a2072502131209c88306371e3855e00c854807
Niloufar Alipour Tehrani <nalipour@cern.ch>
2018-05-15 13:35:51 +0200

    Deep NN with a high-level API from Tensorflow

--- 10_Introduction2ArtificialNN/ANN.py
+++ 10_Introduction2ArtificialNN/ANN.py
@@ -1,6 +1,7 @@
-import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.linear_model import Perceptron
+import numpy as np
+import tensorflow as tf
 
 iris = load_iris()
 X = iris.data[:, (2, 3)]  # petal length and petal width
@@ -11,3 +12,33 @@ y = (iris.target == 0).astype(np.int)  # Iris setosa ?
 per_clf = Perceptron(random_state=42)
 per_clf.fit(X, y)
 y_pred = per_clf.predict([[2, 0.5]])
+
+
+# ### FNN (Feedforward Neural Network) ### #
+# High-level API
+(X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+np.shape(X_train)
+X_train = X_train.astype(np.float32).reshape(-1, 28 * 28) / 255.0
+np.shape(X_train)
+X_test = X_test.astype(np.float32).reshape(-1, 28 * 28) / 255.0
+np.shape(y_train)
+y_train = y_train.astype(np.int32)
+y_test = y_test.astype(np.int32)
+X_valid, X_train = X_train[:5000], X_train[5000:]
+y_valid, y_train = y_train[:5000], y_train[5000:]
+
+feature_cols = [tf.feature_column.numeric_column('X', shape=[28*28])]
+dnn_clf = tf.estimator.DNNClassifier(hidden_units=[300, 100], n_classes=10,
+                                     feature_columns=feature_cols)
+
+input_fn = tf.estimator.inputs.numpy_input_fn(x={'X': X_train}, y=y_train,
+                                              num_epochs=40, batch_size=50,
+                                              shuffle=True)
+dnn_clf.train(input_fn=input_fn)
+
+test_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={'X': X_test}, y=y_test, shuffle=False)
+eval_results = dnn_clf.evaluate(input_fn=test_input_fn)
+y_pred_iter = dnn_clf.predict(input_fn=test_input_fn)
+y_pred = list(y_pred_iter)
+y_pred[0]
