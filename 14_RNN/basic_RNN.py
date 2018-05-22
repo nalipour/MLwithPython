6b0abbab9d748f7bc976e7942fca36b48110baf6
Niloufar Alipour Tehrani <nalipour@cern.ch>
2018-05-22 09:34:00 +0200

    Same RNN sequence but use static_rnn

--- 14_RNN/basic_RNN.py
+++ 14_RNN/basic_RNN.py
@@ -32,3 +32,14 @@ with tf.Session() as sess:
 
 print(Y0_val)
 print(Y1_val)
+
+
+# ### Static Unrolling Through Time ### #
+X0 = tf.placeholder(tf.float32, [None, n_inputs])
+X1 = tf.placeholder(tf.float32, [None, n_inputs])
+
+basic_cell = tf.contrib.rnn.BasicRNNCell(num_units=n_neurons)
+output_seqs, states = tf.contrib.rnn.static_rnn(basic_cell, [X0, X1],
+                                                dtype=tf.float32)
+Y0, Y1 = output_seqs
+print(Y0)
