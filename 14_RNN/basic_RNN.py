b560332e2fae235003382a3b9909ffa6553460f4
Niloufar Alipour Tehrani <nalipour@cern.ch>
2018-05-27 11:23:57 +0200

    dynamic RNN to solve memory problems

--- 14_RNN/basic_RNN.py
+++ 14_RNN/basic_RNN.py
@@ -1,6 +1,13 @@
 import tensorflow as tf
 import numpy as np
 
+
+def reset_graph(seed=42):
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+
+
 '''
 2 layer feedforward neural network: same weights and bias terms are shared by
 both layers. Feed inputs at each layer and get output at each layer
@@ -43,3 +50,62 @@ output_seqs, states = tf.contrib.rnn.static_rnn(basic_cell, [X0, X1],
                                                 dtype=tf.float32)
 Y0, Y1 = output_seqs
 print(Y0)
+
+
+# ### Dynamic unrolling through time ### # ##### THE SOLUTION TO USE
+n_steps = 2
+n_inputs = 3
+n_neurons = 5
+
+reset_graph()
+
+X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+
+basic_cell = tf.contrib.rnn.BasicRNNCell(num_units=n_neurons)
+outputs, states = tf.nn.dynamic_rnn(basic_cell, X, dtype=tf.float32)
+init = tf.global_variables_initializer()
+
+X_batch = np.array([
+        [[0, 1, 2], [9, 8, 7]],  # instance 1
+        [[3, 4, 5], [0, 0, 0]],  # instance 2
+        [[6, 7, 8], [6, 5, 4]],  # instance 3
+        [[9, 0, 1], [3, 2, 1]],  # instance 4
+    ])
+
+with tf.Session() as sess:
+    init.run()
+    outputs_val = outputs.eval(feed_dict={X: X_batch})
+
+print(outputs_val)
+
+# ### Variable input sequences ### #
+n_steps = 2
+n_inputs = 3
+n_neurons = 5
+
+reset_graph()
+
+X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+basic_cell = tf.contrib.rnn.BasicRNNCell(num_units=n_neurons)
+seq_length = tf.placeholder(tf.int32, [None])
+outputs, states = tf.nn.dynamic_rnn(basic_cell, X, dtype=tf.float32,
+                                    sequence_length=seq_length)
+
+init = tf.global_variables_initializer()
+X_batch = np.array([
+        # step 0     step 1
+        [[0, 1, 2], [9, 8, 7]],  # instance 1
+        [[3, 4, 5], [0, 0, 0]],  # instance 2 (padded with zero vectors)
+        [[6, 7, 8], [6, 5, 4]],  # instance 3
+        [[9, 0, 1], [3, 2, 1]],  # instance 4
+    ])
+seq_length_batch = np.array([2, 1, 2, 2])
+
+with tf.Session() as sess:
+    init.run()
+    outputs_val, states_val = sess.run(
+        [outputs, states], feed_dict={X: X_batch,
+                                      seq_length: seq_length_batch})
+
+print(outputs_val)
+print(states_val)
